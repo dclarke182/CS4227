43b0e508e15ef0dfe3edf91eaf37b26389219e11
Daniel <clarkedaniel85@gmail.com>
2022-03-03 15:40:14 +0000

    Test

---
+++ Command/item.java
@@ -0,0 +1,18 @@
+package Command;
+
+class Item {
+    String name;
+
+    public Item(String itemName) {
+        name = itemName;
+    }
+
+    public void itemVisibilityOn() {
+        System.out.println(name + " is now visible");
+    }
+
+    public void itemVisibilityOff() {
+        System.out.println(name + " is now invisible");
+    }
+    //
+}
