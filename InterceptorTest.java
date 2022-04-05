3af25f08888233a761121245324a9fc53a991366
Brian Shanahan <brianshan15@gmail.com>
2022-04-05 20:47:42 +0100

    Visitor

--- InterceptorTest.java
+++ InterceptorTest.java
@@ -1,14 +1,15 @@
 import Interceptor.*;
 
 public class InterceptorTest {
-    public static void main(String[] args) {
-       Dispatcher filterController = new Dispatcher(new Target());
-       filterController.setFilter(new Authentication());
-       filterController.setFilter(new RequestLogger());
-       //filterController.setFilter(new RequestCounter());
+   public static void main(String[] args) {
+      Dispatcher filterController = new Dispatcher(new Target());
+      filterController.setFilter(new Authentication());
+      filterController.setFilter(new RequestLogger());
+      // filterController.setFilter(new RequestCounter());
 
-       Request request = new Request();
-       request.setFilterManager(filterController);
-       request.sendRequest("Login");
-    }
- }
+      Request request = new Request();
+      request.setFilterManager(filterController);
+      request.sendRequest("Login");
+   }
+   //
+}
